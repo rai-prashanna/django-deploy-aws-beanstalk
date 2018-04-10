99398b0f1d1ba0aa211492901477f3610f9400f9
rai-prashannna <prashanna@fusemachines.com>
2018-04-10 13:42:14 +0545

    sample of django project to deploy in beanstalk

---
+++ README.md
@@ -0,0 +1,1 @@
+# django-deploy-aws-beanstalk
---
+++ eb-virt/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/fm-lt-46/workspace/python-workspace/django-basic-aws-template/eb-virt/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
